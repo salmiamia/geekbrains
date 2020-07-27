a827635845d903b8a0ee480b06ecf588a4ae00ec
Liudmila Ermakova <l.eroshkina@gmail.com>
2020-07-28 02:58:51 +0300

    Task4

--- src/edu/quarter_one/lesson_one/Lesson1.java
+++ src/edu/quarter_one/lesson_one/Lesson1.java
@@ -5,7 +5,8 @@ public class Lesson1 {
     public static void main(String[] args) {
         System.out.println(taskOne(4,5,8,3));
         System.out.println(taskTwo(7, 14));
-
+        System.out.println(taskThree(0));
+        System.out.println(taskFour("Вася"));
     }
 
     //Task 1
@@ -14,11 +15,26 @@ public class Lesson1 {
     }
 
     //Task 2
-    public static boolean taskTwo(int a, int b){
+    public static boolean taskTwo(int a, int b) {
         int sum = a + b;
         return sum >= 10 & sum <= 20;
     }
 
+    //Task 3
+    public static String taskThree(int a) {
+        if (a >= 0) return (a + " is positive number");
+        else return (a + " is negative number");
+    }
+
+    //Task 4
+    public static String taskFour(String a) {
+        return "Привет, " + a + "!";
+    }
+
+
+    //*Написать метод, который определяет является ли год високосным.
+    // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+    // Для проверки работы вывести результаты работы метода в консоль.
 
 }
 
